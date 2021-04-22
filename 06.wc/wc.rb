e0263e4a565ce8148a6c6316e81ffaf129196280
shirotamaki <shiro.tamaki63@gmail.com>
2021-04-22 12:07:54 +0900

    全体的に内容を修正し再提出

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -7,7 +7,7 @@ def main
   files = ARGV
   options = ARGV.getopts('l')
   if options['l']
-    operation_option_l(files)
+    operation_l_option(files)
   else
     operation(files)
   end
@@ -20,34 +20,31 @@ def operation(files)
   end
   files.map do |file|
     if FileTest.directory?(file)
-      output_dir(file)
+      output_dir_error(file)
     elsif FileTest.file?(file)
       output_single_file(file)
     else
-      output_error(files)
+      output_error(file)
     end
   end
-  return unless files.last && files.count > 1
-
-  output_plural_files(files)
+  total_files(files)
 end
 
-def operation_option_l(files)
+def operation_l_option(files)
   if files.empty?
     input = $stdin.read
-    output_l_option(input)
+    output_stdin_l_option(input)
   end
   files.map do |file|
     if FileTest.directory?(file)
-      output_dir_option_l(file)
+      output_dir_error_l(file)
     elsif FileTest.file?(file)
-      output_single_file_option_l(file)
+      output_single_file_l_option(file)
     else
       output_error(files)
     end
   end
-  return unless files.last && files.count > 1
-  output_plural_files_option_l(files)
+  total_files_l_option(files)
 end
 
 # 部品 (行数、単語数、バイト数を数える)
@@ -79,20 +76,36 @@ def count_total_bytesizes(files)
   files.sum { |file| count_bytesize(file) }
 end
 
-# 部品（エラーメッセージ等）
-#
-def output_dir(file)
+# 部品（トータル数算出用のファイルを生成）
+def total_files(files)
+  total_files = []
+  files.each do |file|
+    total_files << file if FileTest.file?(file)
+  end
+  output_plural_files(total_files) if files.count > 1
+end
+
+def total_files_l_option(files)
+  total_files = []
+  files.each do |file|
+    total_files << file if FileTest.file?(file)
+  end
+  output_plural_files_l_option(total_files) if files.count > 1
+end
+
+# 部品（エラーメッセージ用）
+def output_dir_error(file)
   puts "wc: #{file}: Is a directory"
   puts "0\s\s0\s\s0\s\s#{file}"
 end
 
-def output_l_dir(file)
+def output_dir_error_l(file)
   puts "wc: #{file}: Is a directory"
   puts "0\s#{file}"
 end
 
-def output_error(files)
-  print "wc: #{files}: No such file or directory\n"
+def output_error(file)
+  print "wc: #{file}: No such file or directory\n"
 end
 
 # 部品（アウトプット用）
@@ -103,42 +116,32 @@ def output_single_file(file)
   print "#{file}\n".to_s
 end
 
-def output_plural_files(files)
-  print count_total_lines(files).to_s.ljust(8)
-  print count_total_words(files).to_s.ljust(8)
-  print count_total_bytesizes(files).to_s.ljust(8)
+def output_plural_files(total_files)
+  print count_total_lines(total_files).to_s.ljust(8)
+  print count_total_words(total_files).to_s.ljust(8)
+  print count_total_bytesizes(total_files).to_s.ljust(8)
   print "total\n".to_s
 end
 
-def output_all_total(files)
-  print count_total_lines(files).ljust(8)
+def output_single_file_l_option(file)
+  print count_line(file).to_s.ljust(8)
+  print "#{file}\n".to_s
+end
+
+def output_plural_files_l_option(files)
+  print count_total_lines(files).to_s.ljust(8)
   print "total\n".to_s
 end
 
 def output_stdin(input)
   print input.count("\n").to_s.ljust(8)
   print input.split(/\s+/).size.to_s.ljust(8)
-  print input.bytesize.to_s.ljust(8)
+  print input.bytesize.to_s.ljust(8) + "\n"
 end
 
-def output_l_option(input)
-  print input.count("\n").to_s.ljust(8)
+def output_stdin_l_option(input)
+  print input.count("\n").to_s
   print "D\n".to_s
-end
-
-def output_dir_option_l(file)
-  print count_line(file).to_s.ljust(8)
-  print "#{file}\n".to_s
-end
-
-def output_single_file_option_l(file)
-  print count_line(file).to_s.ljust(8)
-  print "#{file}\n".to_s
-end
-
-def output_plural_files_option_l(files)
-  print count_total_lines(files).to_s.ljust(8)
-  print "total\n".to_s
 end
 
 main
